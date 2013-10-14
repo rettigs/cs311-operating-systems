0c39d469318b6ecfbe44b72b76f158f79c85b459
Sean Rettig <seanrettig@gmail.com>
2013-10-14 14:12:13 -0500

    Finishing python #1

--- a1/script.py
+++ a1/script.py
@@ -22,10 +22,11 @@ def main():
                                 t = None
                                 c = None
                                 for o, a in opts:
-                                        if o == "-t" or o == "--term":
-                                                t = a
-                                        elif o == "-c" or "--course":
-                                                c = a
+                                        if a is not None:
+                                                if o == "-t" or o == "--term":
+                                                        t = a
+                                                elif o == "-c" or o == "--course":
+                                                        c = a
                                 if t is None or c is None:
                                         print "-t or --term must specify a term and -c or --course must specify a course"
                                 else:
@@ -36,7 +37,22 @@ def main():
                                 print "-i must specify an integer between 1 and 5"
 
 def a1(t, c):
-        print "Creating course directories for term '%s' and course '%s'" % (t, c)
+        dirs = "assignments examples exams lecture_notes submissions".split()
+        for d in dirs:
+                try:
+                        os.makedirs("./%s/%s/%s" % (t, c, d))
+                except OSError:
+                        pass
+        
+        try:
+                os.symlink("/usr/local/classes/eecs/%s/%s/public_html" % (t, c), "website")
+        except OSError:
+                pass
+        try:
+                os.symlink("/usr/local/classes/eecs/%s/%s/handin" % (t, c), "handin")
+        except OSError:
+                pass
+        print "Created course directories for term '%s' and course '%s'" % (t, c)
 
 def a2():
         a = array("c", "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450")
