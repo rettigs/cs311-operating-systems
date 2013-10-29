6ddc0aacbeba29d2ef109a5fe74b8f3434cccdfe
Sean Rettig <seanrettig@gmail.com>
2013-10-29 09:04:15 -0500

    Done with myar arguments

--- a2/myar.c
+++ a2/myar.c
@@ -4,8 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 #include <ar.h>
 #include "myar.h"
+
+char *name; /* Name of the program */
 
 int main(int argc, char *argv[])
 {
@@ -21,45 +24,54 @@ int main(int argc, char *argv[])
         while ((opt = getopt(argc, argv, "qxtdAwv")) != -1) {
                 switch (opt) {
                         case 'q':
-                                q = 1;
+                                q += 1;
                                 break;
                         case 'x':
-                                x = 1;
+                                x += 1;
                                 break;
                         case 't':
-                                t = 1;
+                                t += 1;
                                 break;
                         case 'd':
-                                d = 1;
+                                d += 1;
                                 break;
                         case 'A':
-                                A = 1;
+                                A += 1;
                                 break;
                         case 'w':
-                                w = 1;
+                                w += 1;
                                 break;
                         case 'v':
-                                v = 1;
+                                v += 1;
                                 break;
                         default: /* '?' */
-                                usage(argv[0]);
+                                usage();
                 }
         }
 
         /* Make sure they specified exactly one non-v flag */
-        if (q + x + t + d + A + w != 1) usage(argv[0]);
+        if (q + x + t + d + A + w != 1) usage();
 
-        /* Parse the arguments and create file descriptors for each file */
-        int fd = 
+        /* Make sure they didn't specify the v flag multiple times */
+        if (v > 1) usage();
+
+        /* Strip the flags from the arguments */
+        int argc2 = argc - optind;
+        char **argv2 = &argv[optind];
 
         /* Perform the action indicated by the given flag. */
-        if (q) q(fd, fds);
+        if (q) fq(argc2, argv2);
+        else if (x) fx(argc2, argv2);
+        else if (t) ft(argc2, argv2);
+        else if (d) fd(argc2, argv2);
+        else if (A) fA(argc2, argv2);
+        else if (w) fw(argc2, argv2);
 
         exit(EXIT_SUCCESS);
 }
 
 /* Print usage info and exit */
-void usage(char *name)
+void usage()
 {
         printf("Usage: %s -<q|x|t|d|A|w>[v] <archive> [file ...]\n",
         name);
@@ -67,37 +79,41 @@ void usage(char *name)
 }
 
 /* Print table of contents */
-void q(int fd, int fds[])
+void fq(int argc, char *argv[])
+{
+        if (argc < 2) usage();
+}
+
+/* Print table of contents */
+void fx(int argc, char *argv[])
 {
         
 }
 
 /* Print table of contents */
-void x(int fd)
+void ft(int argc, char *argv[])
+{
+        if (argc != 1) usage();
+
+        for(int i = 0; i < argc; i++){
+                printf("%s\n", argv[i]);
+        }
+}
+
+/* Print table of contents */
+void fd(int argc, char *argv[])
 {
         
 }
 
 /* Print table of contents */
-void t(int fd)
+void fA(int argc, char *argv[])
 {
         
 }
 
 /* Print table of contents */
-void d(int fd)
-{
-        
-}
-
-/* Print table of contents */
-void A(int fd)
-{
-        
-}
-
-/* Print table of contents */
-void w(int fd)
+void fw(int argc, char *argv[])
 {
         
 }
--- a2/myar.h
+++ a2/myar.h
@@ -1,2 +1,7 @@
-void usage(char*);
-void q(int, int[]);
+void usage();
+void fq(int argc, char *argv[]);
+void fx(int argc, char *argv[]);
+void ft(int argc, char *argv[]);
+void fd(int argc, char *argv[]);
+void fA(int argc, char *argv[]);
+void fw(int argc, char *argv[]);
