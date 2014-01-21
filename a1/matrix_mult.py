954808532eb3b47bc03e0da66bb6e81dac02a3d9
Sean Rettig <seanrettig@gmail.com>
2014-01-21 00:46:27 -0600

    done with matrix_mult.py

---
+++ a1/matrix_mult.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import getopt
+import math
+import numpy
+
+def matrix_mult(a, b):
+    arows = a.shape[0]
+    acols = a.shape[1]
+    brows = b.shape[0]
+    bcols = b.shape[1]
+
+    if acols != brows:
+        return "Cannot multiply matrices: size mismatch."
+    else:
+        rows = arows
+        cols = bcols
+        iters = acols # = brows
+        c = [[0 for x in range(cols)] for y in range(rows)]
+
+        for i in range(rows):
+            for j in range(cols):
+                for k in range(iters):
+                    print "c[%d][%d] += a[%d][%d] * b[%d][%d]" % (i, j, i, k, k, j)
+                    c[i][j] += a[i][k] * b[k][j]
+        return c
+
+if __name__ == '__main__':
+    a = numpy.array([[1, 2, 3],
+                     [4, 5, 6]])
+    b = numpy.array([[1, 2],
+                     [3, 4],
+                     [5, 6]])
+    print matrix_mult(a, b)
