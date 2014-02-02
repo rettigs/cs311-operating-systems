0c7e29482623fb3e88ade402a7d99e151eb5b964
Sean Rettig <seanrettig@gmail.com>
2014-02-02 17:09:47 +0000

    initial commit of bdiff, basic functionality should be done but still requires testing

--- a2/bdiff.h
+++ a2/bdiff.h
@@ -1,7 +1,2 @@
 void usage();
-void fq(int argc, char *argv[], int v);
-void fx(int argc, char *argv[], int v);
-void ft(int argc, char *argv[], int v);
-void fd(int argc, char *argv[], int v);
-void fA(int argc, char *argv[], int v);
-void fw(int argc, char *argv[], int v);
+int diff(char *file1, char *file2);
