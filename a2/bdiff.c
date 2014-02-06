2632abe526336dfc075268b2df8cab51fca638da
Sean Rettig <seanrettig@gmail.com>
2014-02-06 05:01:53 +0000

    make bdiff -b print special characters properly

--- a2/bdiff.c
+++ a2/bdiff.c
@@ -89,6 +89,27 @@ int diff(char *file1, char *file2)
         if(fstat(fd1, &statbuf1) == -1 || fstat(fd2, &statbuf2) == -1) error("Could not stat file");
         int pad = ceil(log10(MAX(statbuf1.st_size, statbuf2.st_size)));
 
+        /* Lookup table so we can print special characters */
+        char *chars[256] = { "^@", "^A", "^B", "^C", "^D", "^E", "^F", "^G", "^H", "^I", "^J", "^K", "^L", "^M", "^N", 
+        "^O", "^P", "^Q", "^R", "^S", "^T", "^U", "^V", "^W", "^X", "^Y", "^Z", "^[", "^\\", "^]", 
+        "^^", "^_", " ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", 
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", 
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", 
+        "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", 
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
+        "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~", "^?", 
+        "M-^@", "M-^A", "M-^B", "M-^C", "M-^D", "M-^E", "M-^F", "M-^G", "M-^H", "M-^I", "M-^J", 
+        "M-^K", "M-^L", "M-^M", "M-^N", "M-^O", "M-^P", "M-^Q", "M-^R", "M-^S", "M-^T", "M-^U", 
+        "M-^V", "M-^W", "M-^X", "M-^Y", "M-^Z", "M-^[", "M-^\\", "M-^]", "M-^^", "M-^_", "M- ", 
+        "M-!", "M-\"", "M-#", "M-$", "M-%", "M-&", "M-'", "M-(", "M-)", "M-*", "M-+", "M-,", "M--", 
+        "M-.", "M-/", "M-0", "M-1", "M-2", "M-3", "M-4", "M-5", "M-6", "M-7", "M-8", "M-9", "M-:", 
+        "M-;", "M-<", "M-=", "M->", "M-?", "M-@", "M-A", "M-B", "M-C", "M-D", "M-E", "M-F", "M-G", 
+        "M-H", "M-I", "M-J", "M-K", "M-L", "M-M", "M-N", "M-O", "M-P", "M-Q", "M-R", "M-S", "M-T", 
+        "M-U", "M-V", "M-W", "M-X", "M-Y", "M-Z", "M-[", "M-\\", "M-]", "M-^", "M-_", "M-`", "M-a", 
+        "M-b", "M-c", "M-d", "M-e", "M-f", "M-g", "M-h", "M-i", "M-j", "M-k", "M-l", "M-m", "M-n", 
+        "M-o", "M-p", "M-q", "M-r", "M-s", "M-t", "M-u", "M-v", "M-w", "M-x", "M-y", "M-z", "M-{", 
+        "M-|", "M-}", "M-~", "M-^?" };
+
         int read1 = 0;
         int read2 = 0;
         int byteindex = 0;
@@ -104,7 +125,7 @@ int diff(char *file1, char *file2)
                                 if(buf1[i] != buf2[i]){
                                         status = 1;
                                         if(!quiet){
-                                                if(verbose) printf("%*d %o %c    %o %c\n", pad, byteindex, buf1[i], buf1[i], buf2[i], buf2[i]);
+                                                if(verbose) printf("%*d %3o %-4s %3o %s\n", pad, byteindex, buf1[i], chars[buf1[i]], buf2[i], chars[buf2[i]]);
                                                 else printf("%*d %3o %3o\n", pad, byteindex, buf1[i], buf2[i]);
                                         }
                                 }
