96686b05b16246ad808dbc422214c1a87c1a665a
Sean Rettig <seanrettig@gmail.com>
2014-02-20 18:28:16 -0800

    score program reformatting, finish parsing flags and args

--- a3/score.c
+++ a3/score.c
@@ -15,100 +15,115 @@
 #define FP_SPECIAL 1
 
 char *name; // Name of program
+int DEBUG = 0; // Whether we are in debug mode
 
 int main(int argc, char *argv[])
 {
-        name = argv[0];
+    name = argv[0];
 
-        // Parse flags
-        int opt, threads, infd, outfd;
-        threads = 1;
-        infd = 0;
-        outfd = 1;
-        while ((opt = getopt(argc, argv, "n:")) != -1) {
-                switch (opt) {
-                        case 'n':
-                                if (sscanf(optarg, "%d", &threads) != 1) usage();
-                                break;
-                        default: // '?'
-                                usage();
-                }
+    /* Parse flags */
+    int opt, threads, outfd;
+    threads = 1;
+    outfd = 1;
+    while((opt = getopt(argc, argv, "t:o:d")) != -1){
+        char *outfile = "";
+        switch(opt){
+            case 't':
+                if(sscanf(optarg, "%d", &threads) != 1) usage();
+                break;
+            case 'o':
+                if(sscanf(optarg, "%s", outfile) != 1) usage();
+                if((outfd = open(outfile, O_WRONLY | O_TRUNC | O_CREAT, 0644)) == -1) error("Could not open output file");
+                break;
+            case 'd':
+                DEBUG = 1;
+                break;
+            default: // '?'
+                usage();
         }
-        if (threads < 1) {
-                printf("Error: Must specify at least 1 scorer thread\n");
-                exit(EXIT_FAILURE);
-        }
-        if (infd == -1) error("Error: Could not open input file");
-        if (outfd == -1) error("Error: Could not open output file");
+    }
+    if(threads < 1) error("Must specify at least 1 scorer thread\n");
 
-        /* Create reader -> scorers pipes */
-        int rtospipe[threads][2];
-        for(int i = 0; i < threads; i++) {
-            if(pipe(&rtospipe[i]) == -1) error("Error: Could not create reader -> scorer pipe");
-        }
+    /* Strip the flags from the arguments */
+    int argc2 = argc - optind;
+    char **argv2 = &argv[optind];
 
-        /* Create scorers -> combiner pipe */
-        int stocpipe[2];
-        if(pipe(&stocpipe) == -1) error("Error: Could not create scorer -> combiner pipe");
+    /* Create reader -> scorers pipes */
+    int rtospipe[threads][2];
+    for(int i = 0; i < threads; i++) {
+        if(pipe((int *) &rtospipe[i]) == -1) error("Error: Could not create reader -> scorer pipe");
+    }
 
-        /* Fork off reader process */
-        pid_t rpid;
-        switch(rpid = fork()){
+    /* Create scorers -> combiner pipe */
+    int stocpipe[2];
+    if(pipe((int *) &stocpipe) == -1) error("Error: Could not create scorer -> combiner pipe");
+
+    /* Fork off reader process */
+    pid_t rpid;
+    switch(rpid = fork()){
+        case -1: // Error case
+            error("Could not fork off reader process");
+        case 0: // Child case
+            reader(threads, rtospipe, argc2, argv2);
+            _exit(EXIT_SUCCESS);
+    } // Parent continues
+
+    /* Fork off scorer processes */
+    pid_t spid[threads];
+    for(int i = 0; i < threads; i++){
+        switch(spid[i] = fork()){
             case -1: // Error case
-                error("Could not fork off reader process");
+                error("Could not fork off scorer process");
             case 0: // Child case
-                reader();
+                scorer();
+                _exit(EXIT_SUCCESS);
         } // Parent continues
+    }
 
-        /* Fork off scorer processes */
-        pid_t spid[threads];
-        for(int i = 0; i < threads; i++){
-            switch(spid[i] = fork()){
-                case -1: // Error case
-                    error("Could not fork off scorer process");
-                case 0: // Child case
-                    scorer();
-            } // Parent continues
-        }
+    /* Become the combiner process */
+    combiner();
 
-        /* Become the combiner process */
-        combiner();
-
-        exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS);
 }
 
 /* Performs the task of the reader process */
-void reader()
+void reader(int threads, int (*rtospipe)[2], int filec, char **filev)
 {
-    printf("Reader spawned; closing\n");
-    _exit(0);
+    debug("Reader spawned");
+
+    for(int i = 0; i < filec; i++){
+        
+    }
 }
 
 /* Performs the task of a scorer process */
 void scorer()
 {
     printf("Scorer spawned; closing\n");
-    _exit(0);
 }
 
 /* Performs the task of the combiner process */
 void combiner()
 {
     printf("Combiner started; finishing\n");
-    _exit(0);
 }
 
 /* Print usage info and exit */
 void usage()
 {
-        printf("Usage: %s -t <#ofsortthreads> [-i infile] [-o outfile]\n",
-        name);
-        exit(EXIT_FAILURE);
+    printf("Usage: %s [-t scorer_threads] [-o outfile] [-d] FILE...\n", name);
+    exit(EXIT_FAILURE);
 }
 
 /* Print given error message and exit */
 void error(char *message)
 {
-        perror(message);
-        exit(EXIT_FAILURE);
+    perror(message);
+    exit(EXIT_FAILURE);
+}
+
+/* Print given message if debug flag is set */
+void debug(char *message)
+{
+    if(DEBUG) printf("%s\n", message);
 }
--- a3/score.h
+++ a3/score.h
@@ -1,5 +1,6 @@
-void reader();
+void reader(int, int (*)[2],  int, char **);
 void scorer();
 void combiner();
 void usage();
 void error(char *);
+void debug(char *);
