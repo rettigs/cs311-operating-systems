1ff841c6b126729e6b9bad788712ffa7b1df3264
Sean Rettig <seanrettig@gmail.com>
2014-02-20 17:12:28 -0800

    done with basic pipe creation and forking in score.c

--- a3/score.c
+++ a3/score.c
@@ -10,7 +10,7 @@
 #include <getopt.h>
 #include <string.h>
 #include <dirent.h>
-#include "uniqify.h"
+#include "score.h"
 
 #define FP_SPECIAL 1
 
@@ -41,71 +41,61 @@ int main(int argc, char *argv[])
         if (infd == -1) error("Error: Could not open input file");
         if (outfd == -1) error("Error: Could not open output file");
 
-        /* Create reader -> scorer pipes */
-        int rtos[threads][2];
-        for (int i = 0; i < threads; i++) {
-                rtos[i] = pipefds;
-                if (pipe(&rtos[i]) == -1) error("Error: Could not create reader -> scorer pipe");
+        /* Create reader -> scorers pipes */
+        int rtospipe[threads][2];
+        for(int i = 0; i < threads; i++) {
+            if(pipe(&rtospipe[i]) == -1) error("Error: Could not create reader -> scorer pipe");
+        }
 
-        /* Spin off reader process */
+        /* Create scorers -> combiner pipe */
+        int stocpipe[2];
+        if(pipe(&stocpipe) == -1) error("Error: Could not create scorer -> combiner pipe");
+
+        /* Fork off reader process */
         pid_t rpid;
         switch(rpid = fork()){
-            case -1: // error case
-                error("Could not fork reader process");
-            case 0: // child case
+            case -1: // Error case
+                error("Could not fork off reader process");
+            case 0: // Child case
                 reader();
-        } // parent continues
+        } // Parent continues
 
-        /* Spin off scorer processes */
-        pid_t spid[thread];
+        /* Fork off scorer processes */
+        pid_t spid[threads];
         for(int i = 0; i < threads; i++){
             switch(spid[i] = fork()){
-                case -1: // error case
-                    error("Could not fork scorer process");
-                case 0: // child case
+                case -1: // Error case
+                    error("Could not fork off scorer process");
+                case 0: // Child case
                     scorer();
-            } // parent continues
-        }
-        switch(rpid = fork()){
-            case -1: // error case
-                error("could not fork reader process");
-            case 0: // child case
-                reader();
-        } // parent continues
-
-
-        // Create tosort pipes
+            } // Parent continues
         }
 
-        // Close read pipes
-
-        // Fork off children
-        for (int i = 0; i < threads; i++) {
-                switch(fork()) {
-                        case -1: // Error case
-                                error("Could not fork");
-                        case 0: // Child case
-                                close(tosortpipes[i][0]); //Close the input write end.
-                                close(fromsortpipes[i][1]); //Close the output read end.
-                                dup2(0, tosortpipes[i][1]); //Make the input read end the new stdin.
-                                dup2(1, tosortpipes[i][0]); //Make the output write end the new stdout.
-                                //exec
-                }
-        }
-
-        // Close read ends of tosort pipes and write ends of fromsort pipes
-        for (int i = 0; i < threads; i++) {
-                close(tosortpipes[i][1]);
-                close(fromsortpipes[i][0]);
-        }
-
-        // Read and distribute input
-
-        // Close write pipes
-
-        // Write output
+        /* Become the combiner process */
+        combiner();
 
         exit(EXIT_SUCCESS);
+}
+
+/* Performs the task of the reader process */
+void reader()
+{
+    printf("Reader spawned; closing\n");
+    _exit(0);
+}
+
+/* Performs the task of a scorer process */
+void scorer()
+{
+    printf("Scorer spawned; closing\n");
+    _exit(0);
+}
+
+/* Performs the task of the combiner process */
+void combiner()
+{
+    printf("Combiner started; finishing\n");
+    _exit(0);
 }
 
 /* Print usage info and exit */
