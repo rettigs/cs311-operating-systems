a4bcf2a400eb918c04ed3142b7ae9e9df663aa14
Sean Rettig <seanrettig@gmail.com>
2014-03-05 17:32:17 -0800

    fix all debug statements in trouter to show thread id

--- a4/trouter.c
+++ a4/trouter.c
@@ -216,7 +216,7 @@ void insert(struct trienode *root, char *key, int value)
 {
     if(strlen(key) > 0) __recurseInsert(root, key, value);
     else{
-        if(DEBUG > 1) printf("Insert: key length is 0, placing ASN %d at root\n", value);
+        if(DEBUG > 1) printf("[Worker %d] Insert: key length is 0, placing ASN %d at root\n", (int) pthread_self(), value);
         root->ASN = value;
     }
 }
@@ -234,26 +234,26 @@ void __recurseInsert(struct trienode *root, char *key, int value)
     // Root cannot be null
     char digit = key[0];
 
-    if(DEBUG > 1) printf("Recursive insert: started, digit is %c, key is %s\n", digit, key);
+    if(DEBUG > 1) printf("[Worker %d] Recursive insert: started, digit is %c, key is %s\n", (int) pthread_self(), digit, key);
 
     if(strlen(key) <= 0){
         // We have hit the bottom. We should shove our value at this node.
         if(!root->populated){
             root->ASN = value;
             root->populated = 1;
-        }else if(DEBUG) printf("WARNING: Duplicate assignment: node already contains ASN %d; not adding %d\n", root->ASN, value);
+        }else if(DEBUG) printf("[Worker %d] Recursive insert: duplicate assignment; node already contains ASN %d; not adding %d\n", (int) pthread_self(), root->ASN, value);
         return;
     }
 
     if(digit == '0'){
         if(root->zero == NULL){
-            if(DEBUG > 1) printf("Recursive insert: adding 'zero' node for ASN %d at %s\n", value, key);
+            if(DEBUG > 1) printf("[Worker %d] Recursive insert: adding 'zero' node for ASN %d at %s\n", (int) pthread_self(), value, key);
             root->zero = init_trienode();
         }
         __recurseInsert(root->zero, &key[1], value);
     }else{
         if(root->one == NULL){
-            if(DEBUG > 1) printf("Recursive insert: adding 'one' node for ASN %d at %s\n", value, key);
+            if(DEBUG > 1) printf("[Worker %d] Recursive insert: adding 'one' node for ASN %d at %s\n", (int) pthread_self(), value, key);
             root->one = init_trienode();
         }
         __recurseInsert(root->one, &key[1], value);
@@ -264,33 +264,33 @@ int __recurseSearch(struct trienode *root, char *key, int *valuebuf)
 {
     char digit = key[0];
 
-    if(DEBUG > 1) printf("Recursive search: started, digit is %c, key is %s\n", digit, key);
+    if(DEBUG > 1) printf("[Worker %d] Recursive search: started, digit is %c, key is %s\n", (int) pthread_self(), digit, key);
 
     if(root == NULL){
-        if(DEBUG > 1) printf("Recursive search: root is null at key %s\n", key);
+        if(DEBUG > 1) printf("[Worker %d] Recursive search: root is null at key %s\n", (int) pthread_self(), key);
         return *valuebuf;
     }
 
     if(root->populated){
-        if(DEBUG > 1) printf("Recursive search: root is populated with ASN %d at key %s\n", root->ASN, key);
+        if(DEBUG > 1) printf("[Worker %d] Recursive search: root is populated with ASN %d at key %s\n", (int) pthread_self(), root->ASN, key);
         valuebuf = &root->ASN;
     }
 
     if(strlen(key) <= 0){
         // We're done!
-        if(DEBUG > 1) printf("Recursive search: key exhausted, returning ASN %d\n", root->ASN);
+        if(DEBUG > 1) printf("[Worker %d] Recursive search: key exhausted, returning ASN %d\n", (int) pthread_self(), root->ASN);
         return *valuebuf;
     }
 
     if(digit == '0'){
         if(root->zero == NULL){
-            if(DEBUG > 1) printf("Recursive search: node at key %s has no 'zero' child, returning ASN %d\n", key, root->ASN);
+            if(DEBUG > 1) printf("[Worker %d] Recursive search: node at key %s has no 'zero' child, returning ASN %d\n", (int) pthread_self(), key, root->ASN);
             return *valuebuf;
         }
         else return __recurseSearch(root->zero, &key[1], valuebuf);
     }else{
         if(root->one == NULL){
-            if(DEBUG > 1) printf("Recursive search: node at key %s has no 'one' child, returning ASN %d\n", key, root->ASN);
+            if(DEBUG > 1) printf("[Worker %d] Recursive search: node at key %s has no 'one' child, returning ASN %d\n", (int) pthread_self(), key, root->ASN);
             return *valuebuf;
         }
         else return __recurseSearch(root->one, &key[1], valuebuf);
